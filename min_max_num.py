dc9135321e0dc46dc686e38a8a83bcd4199378c2
Aigerim Askerbekova <askerbekowaa>
2025-02-17 22:26:09 +0600

    1

---
+++ min_max_num.py
@@ -0,0 +1,12 @@
+numbers = [22, 45, 67, 12, 89, 34, 55, 90, 10]
+min_num = numbers[0]
+max_num = numbers[0]
+
+for num in numbers:
+    if num > max_num:
+        max_num = num
+    if num < min_num:
+        min_num = num
+        
+print("Наибольшее число: ", max_num)
+print("Наименьшее число: ", min_num)
